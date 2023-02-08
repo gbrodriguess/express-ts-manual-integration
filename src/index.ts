0af74ddace46220eac85be7deab1a141a28e93a1
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-02-08 17:29:05 -0300

    feat: Adding import for controllerRouter

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import express, { Request, Response } from 'express';
-import { router } from './controllers/decorators/controller';
-import bodyParser from 'body-parser';
-import cookieSession from 'cookie-session';
+import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { router as ControllerRouter } from "./controllers/decorators/controller";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: ['laskdjf'] }));
+app.use(cookieSession({ keys: ["laskdjf"] }));
 app.use(router);
+app.use(ControllerRouter)
+
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
