c13e302282b44f3433f2b92da6e6c5c700e3b8a6
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-02-08 17:51:13 -0300

    feat: configuring AppRouter singleton

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,17 +1,17 @@
-import express from 'express';
+import express from "express";
 
-export const router = express.Router();
+import { AppRouter } from "../../AppRouter";
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
+    const router = AppRouter.getInstance();
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata("path", target.prototype, key);
 
-      if(path) {
-        router.get(`${routePrefix}${path}`,routeHandler);
+      if (path) {
+        router.get(`${routePrefix}${path}`, routeHandler);
       }
-
     }
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
-import { router as ControllerRouter } from "./controllers/decorators/controller";
+import { AppRouter } from "./AppRouter";
 
 import "./controllers/LoginController";
 
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["laskdjf"] }));
 app.use(router);
-app.use(ControllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
