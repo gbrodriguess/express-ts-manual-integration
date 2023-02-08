192d50f7a417433e9db14ac1f89c046065b43f7e
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-02-08 17:24:35 -0300

    feat: chaning router path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
+import { router } from './controllers/decorators/controller';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
