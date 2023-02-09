f16d4e1ebf25c1c21236c5169af5f5f22107559f
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-02-09 00:01:25 -0300

    feat: adding enum file

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -9,6 +9,8 @@ export function controller(routePrefix: string) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata("path", target.prototype, key);
 
+      const method = Reflect.getMetadata("method", target.prototype, key);
+
       if (path) {
         router.get(`${routePrefix}${path}`, routeHandler);
       }
